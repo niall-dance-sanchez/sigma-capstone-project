0e31bad38e538bac80c15d47ad23debd3727e839
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-06-20 13:10:51 +0100

    added points for each shootout and fixed faulty score checking system

--- capstone-project.py
+++ capstone-project.py
@@ -83,80 +83,114 @@ def penalty(keeper_stats_avg, taker_stats_avg, goalkeeper, first_taker):
         print(f"You {phrases[3]}")
 
 
-def check_score(user_score, opponent_score, shot_count):
-        '''
-        DEFINITION
-        '''
-        score_dif = abs(user_score-opponent_score)
-        if shot_count // 2 == 3 and score_dif == 3: 
-            return True
-        elif shot_count // 2 == 4 and score_dif >= 2:
-            return True
-        elif shot_count // 2 >= 5 and score_dif >= 1 and shot_count % 2 == 0:
-            return True
+def check_score(user_score, opponent_score, shot_count, first_taker):
+    '''
+    DEFINITION
+    '''
+    if first_taker:
+        user_shot_count = (shot_count+1) // 2
+        opponent_shot_count = shot_count // 2
+    else: 
+        opponent_shot_count = (shot_count+1) // 2
+        user_shot_count = shot_count // 2
 
-taker_stats = [{"Name": "Alvaro Morata", "Age": f"{age_calculator("23-10-1992")}", "Shot power": 55, "Accuracy": 48, "Composure": 27, "Special ability": None},
-                {"Name": "Scott McTominay", "Age": f"{age_calculator("08-12-1996")}", "Shot power": 58, "Accuracy": 42, "Composure": 50, "Special ability": None},
-                {"Name": "Bruno Fernandes", "Age": f"{age_calculator("08-09-1994")}", "Shot power": 60, "Accuracy": 56, "Composure": 64, "Special ability": "Hop penalty"},
-                {"Name": "Andrea Pirlo", "Age": f"{age_calculator("19-05-1979")}", "Shot power": 46, "Accuracy": 75, "Composure": 90, "Special ability": "Panenka penalty"},
-                {"Name": "Crisiano Ronaldo", "Age": f"{age_calculator("05-02-1985")}", "Shot power": 85, "Accuracy": 73, "Composure": 82, "Special ability": "Volley penalty"}]
+    user_remaining = 5 - user_shot_count
+    opponent_remaining = 5 - opponent_shot_count
 
-keeper_stats = [{"Name": "Kepa Arrizabalaga", "Age": f"{age_calculator("03-10-1994")}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": None},
-                {"Name": "David Marshall", "Age": f"{age_calculator("05-03-1985")}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": None},
-                {"Name": "Unai Simon", "Age": f"{age_calculator("11-06-1997")}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": "Early read"},
-                {"Name": "Petr Cech", "Age": f"{age_calculator("20-05-1982")}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": "Safe hands"},
-                {"Name": "Iker Casillas", "Age": f"{age_calculator("20-05-1981")}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": "Cat spring"}]
+    if shot_count < 10 and (user_score > opponent_score + opponent_remaining or opponent_score > user_score + user_remaining):
+        return True
+    elif shot_count >= 10 and shot_count % 2 == 0 and user_score != opponent_score: 
+        return True
+
+    return False
+
+taker_stats = [{"Name": "Alvaro Morata", "Age": f"{age_calculator('23-10-1992')}", "Shot power": 55, "Accuracy": 48, "Composure": 27, "Special ability": None},
+                {"Name": "Scott McTominay", "Age": f"{age_calculator('08-12-1996')}", "Shot power": 58, "Accuracy": 42, "Composure": 50, "Special ability": None},
+                {"Name": "Bruno Fernandes", "Age": f"{age_calculator('08-09-1994')}", "Shot power": 60, "Accuracy": 56, "Composure": 64, "Special ability": "Hop penalty"},
+                {"Name": "Andrea Pirlo", "Age": f"{age_calculator('19-05-1979')}", "Shot power": 46, "Accuracy": 75, "Composure": 90, "Special ability": "Panenka penalty"},
+                {"Name": "Crisiano Ronaldo", "Age": f"{age_calculator('05-02-1985')}", "Shot power": 85, "Accuracy": 73, "Composure": 82, "Special ability": "Volley penalty"}]
+
+keeper_stats = [{"Name": "Kepa Arrizabalaga", "Age": f"{age_calculator('03-10-1994')}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": None},
+                {"Name": "David Marshall", "Age": f"{age_calculator('05-03-1985')}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": None},
+                {"Name": "Unai Simon", "Age": f"{age_calculator('11-06-1997')}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": "Early read"},
+                {"Name": "Petr Cech", "Age": f"{age_calculator('20-05-1982')}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": "Safe hands"},
+                {"Name": "Iker Casillas", "Age": f"{age_calculator('20-05-1981')}", "Diving": 70, "Handling": 58, "Reflexes": 32, "Special ability": "Cat spring"}]
 
 def penalty_shootout():
     '''
     DEFINITION
     '''
-    # prompt user to select their penalty taker and goalkeeper
-    user_taker = player_selection(taker_stats, 'penalty taker')
-    user_keeper = player_selection(keeper_stats, 'goalkeeper')
+    points = 0
+    round_points = 0
+    plays = 0
 
-    # initialise the score and perform the coin toss to decide who takes first
-    user_score = 0
-    opponent_score = 0
-    shot_count = 0
-    first_taker = coin_toss()
-
-    taker_stats_avg = sum(list(user_taker.values())[2:5]) // 3
-    keeper_stats_avg = sum(list(user_keeper.values())[2:5]) // 3
-    list(user_taker.values())[5]
+    print("#"*28 + "\nWelcome to Penalty Shootout!\n" + "#"*28)
 
     while True: 
+        
+        points += round_points
 
-        # take first penalty if the player won the coin toss, 
-        # if not skip this code once as the player is taking second    
-        if (not first_taker and shot_count != 0) or first_taker: 
-            if penalty(keeper_stats_avg, taker_stats_avg, False, first_taker):    
-            #if take_penalty(taker_stats_avg, first_taker):
-                user_score += 1
+        # prompt user to play again after they have played more than once
+        if plays: 
+            play_again = input(f"Total points = {points} \n Would you like to play again? (Type YES to continue or anything else to finish) ").lower()
+            if play_again != 'yes':
+                break
+
+        
+        round_points -= round_points
+
+        # prompt user to select their penalty taker and goalkeeper
+        user_taker = player_selection(taker_stats, "penalty taker")
+        user_keeper = player_selection(keeper_stats, "goalkeeper")
+
+        # initialise the score and perform the coin toss to decide who takes first
+        user_score = 0
+        opponent_score = 0
+        shot_count = 0
+        first_taker = coin_toss()
+
+        taker_stats_avg = sum(list(user_taker.values())[2:5]) // 3
+        keeper_stats_avg = sum(list(user_keeper.values())[2:5]) // 3
+        list(user_taker.values())[5]
+
+        while True: 
+
+            # take first penalty if the player won the coin toss, 
+            # if not skip this code once as the player is taking second    
+            if (not first_taker and shot_count != 0) or first_taker: 
+                if penalty(keeper_stats_avg, taker_stats_avg, False, first_taker):    
+                #if take_penalty(taker_stats_avg, first_taker):
+                    round_points += 100
+                    user_score += 1
+
+                shot_count += 1
+                print(f'You {user_score} - {opponent_score} Opp')
+
+                if check_score(user_score, opponent_score, shot_count, first_taker):
+                    break
+            
+            if not penalty(keeper_stats_avg, taker_stats_avg, True, first_taker):
+            #if not save_penalty(keeper_stats_avg, first_taker):
+                opponent_score += 1 
 
             shot_count += 1
             print(f'You {user_score} - {opponent_score} Opp')
 
-            if check_score(user_score, opponent_score, shot_count):
+            if check_score(user_score, opponent_score, shot_count, first_taker):
                 break
         
-        if not penalty(keeper_stats_avg, taker_stats_avg, True, first_taker):
-        #if not save_penalty(keeper_stats_avg, first_taker):
-            opponent_score += 1 
+        # determine the winner of the shootout
+        scores = [user_score, opponent_score]
+        winner = scores.index(max(scores))
+        plays += 1
 
-        shot_count += 1
-        print(f'You {user_score} - {opponent_score} Opp')
-
-        if check_score(user_score, opponent_score, shot_count):
-            break
+        if winner == 0: 
+            round_points += 200
+            print(f"You win!!! \nRound points: {round_points}")
+        else:
+            round_points -= 200
+            print(f"You lose!!! \nRound points: {round_points}")
     
-    # determine the winner of the shootout
-    scores = [user_score, opponent_score]
-    winner = scores.index(max(scores))
-
-    if winner == 0: 
-        return print("You win!!!")
-    else:
-        return print("You lose!!!")
+    print(f"After {plays} shootouts you scored {points} points!")
     
 penalty_shootout()
